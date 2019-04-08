b7b6da4d17d5497ed52820dcfe6721927e3c6924
Juha Harviainen <juha.harviainen@gmail.com>
2019-04-08 20:54:48 +0300

    Remake main scene ui

--- OKTI/src/main/java/okti/gui/MainScene.java
+++ OKTI/src/main/java/okti/gui/MainScene.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import okti.db.DeckDAO;
 import okti.domain.Deck;
+import okti.event.DeleteDeckButtonClickedEventHandler;
+import okti.event.EditDeckButtonClickedEventHandler;
 import okti.event.NewDeckButtonClickedEventHandler;
 import okti.event.PractiseDeckButtonClickedEventHandler;
 
@@ -16,6 +18,7 @@ public class MainScene extends AppScene {
     private static final int COLUMNS = 5;
     private final int cellSize;
     private DeckDAO deckDAO;
+    private final double heightRatio = 1.0 / 3;
     
     /**
      * Constructor for a deck practise scene.
@@ -33,7 +36,7 @@ public class MainScene extends AppScene {
      * @param grid The grid of the scene
      * @param decks List of the decks.
      */
-    private void addButtons(GridPane grid, List<Deck> decks) {
+    private void addDeckButtons(GridPane grid, List<Deck> decks) {
         int x = 0;
         int y = 0;
         for (Deck deck : decks) {
@@ -41,7 +44,7 @@ public class MainScene extends AppScene {
             button.setMinWidth(cellSize);
             button.setMinHeight(cellSize);
             button.setOnMouseClicked(new PractiseDeckButtonClickedEventHandler(super.getApp(), deck.getId()));
-            grid.add(button, x, y);
+            grid.add(button, 2 * x, 2 * y, 2, 1);
             x = (x + 1) % 5;
             if (x == 0) {
                 y++;
@@ -49,9 +52,35 @@ public class MainScene extends AppScene {
         }
         Button newDeckButton = new Button("Uusi pakka");
         newDeckButton.setMinWidth(cellSize);
-        newDeckButton.setMinHeight(cellSize);
+        newDeckButton.setMinHeight(cellSize + cellSize * heightRatio);
         newDeckButton.setOnMouseClicked(new NewDeckButtonClickedEventHandler(super.getApp()));
-        grid.add(newDeckButton, x, y);
+        grid.add(newDeckButton, 2 * x, 2 * y, 2, 2);
+    }
+    
+    /**
+     * Add edit buttons to the grid.
+     * @param grid The grid of the scene
+     * @param decks List of the decks.
+     */
+    private void addEditButtons(GridPane grid, List<Deck> decks) {
+        int x = 0;
+        int y = 0;
+        for (Deck deck : decks) {
+            Button edit = new Button("Muokkaa");
+            edit.setMinWidth(cellSize / 2);
+            edit.setMinHeight(cellSize * heightRatio);
+            edit.setOnMouseClicked(new EditDeckButtonClickedEventHandler(super.getApp(), deck.getId()));
+            grid.add(edit, 2 * x, 2 * y + 1);
+            Button delete = new Button("Poista");
+            delete.setMinWidth(cellSize / 2);
+            delete.setMinHeight(cellSize * heightRatio);
+            delete.setOnMouseClicked(new DeleteDeckButtonClickedEventHandler(super.getApp(), deck.getId()));
+            grid.add(delete, 2 * x + 1, 2 * y + 1);
+            x = (x + 1) % 5;
+            if (x == 0) {
+                y++;
+            }
+        }
     }
     
     @Override
@@ -59,8 +88,8 @@ public class MainScene extends AppScene {
         List<Deck> decks;
         GridPane grid = new GridPane();
         
-        for (int i = 0; i < COLUMNS; i++) {
-            grid.getColumnConstraints().add(new ColumnConstraints(cellSize));
+        for (int i = 0; i < 2 * COLUMNS; i++) {
+            grid.getColumnConstraints().add(new ColumnConstraints(cellSize / 2));
         }
         
         try {
@@ -69,7 +98,8 @@ public class MainScene extends AppScene {
             decks = new ArrayList<>();
         }
         
-        addButtons(grid, decks);
+        addDeckButtons(grid, decks);
+        addEditButtons(grid, decks);
         
         return new Scene(grid);
     }
